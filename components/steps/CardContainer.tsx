6a7b32301bac2a2761808d7ab9250380cd055990
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-01-12 22:49:58 +0530

    eslint fix

--- components/steps/CardContainer.tsx
+++ components/steps/CardContainer.tsx
@@ -3,12 +3,11 @@ import Card from "./Card";
 import ImageContainer from "../ImageContainer";
 
 const CardContainer = () => {
-
-    const [fundas,setFundas] = useState<File[]>([])
-    const imageCallback = (filesArray: File[]) => {
-      console.log("imageCallback", filesArray)
-      setFundas(filesArray)
-    }
+  const [fundas, setFundas] = useState<File[]>([]);
+  const imageCallback = (filesArray: File[]) => {
+    console.log("imageCallback", filesArray);
+    setFundas(filesArray);
+  };
   return (
     <div className="relative w-full h-full p-5 ">
       <Card index={0} />
@@ -16,17 +15,13 @@ const CardContainer = () => {
       <Card index={2} />
       <Card index={3} />
       <Card title="Crop Roi" index={4} />
-      <Card
-        title="Select Image"
-        index={5}
-        children={
-          <ImageContainer
-            instructions="Drop Your Image Here OR"
-            multiple={false}
-            updateFileCb={imageCallback}
-          />
-        }
-      />
+      <Card title="Select Image" index={5}>
+        <ImageContainer
+          instructions="Drop Your Image Here OR"
+          multiple={false}
+          updateFileCb={imageCallback}
+        />
+      </Card>
     </div>
   );
 };
