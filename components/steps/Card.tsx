cbebb6befbeb783df522b4a473f5e496184933eb
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-01-12 23:26:58 +0530

    tailwinnd fix

--- components/steps/Card.tsx
+++ components/steps/Card.tsx
@@ -9,7 +9,14 @@ const Card: FunctionComponent<{
 }> = ({ index, children, title, showNext }) => {
   return (
     <div
-      className={`flex flex-col items-center absolute bg-white h-96 top-${index} border p-5 m-5 rounded-t-lg border-black left-0 right-0 bottom-0`}
+      className={`flex flex-col items-center absolute bg-white h-96 ${
+        index === 0 ? "top-0" :
+        index === 1 ? "top-1" :
+        index === 2 ? "top-2" :
+        index === 3 ? "top-3" :
+        index === 4 ? "top-4" :
+        "top-5" 
+      } border p-5 m-5 rounded-t-lg border-black left-0 right-0 bottom-0`}
     >
       {!showNext ? (
         <>
