520f5460caade4e2fdbe9943a3f2d8ba29e0d53d
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-01-13 22:44:24 +0530

    time for card 2

--- components/steps/Card.tsx
+++ components/steps/Card.tsx
@@ -1,30 +1,46 @@
-import React, { FunctionComponent, ReactNode } from "react";
+import React, {
+  FunctionComponent,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
 import Button from "../Button";
 
 const Card: FunctionComponent<{
   showNext?: boolean;
   title?: string;
+  handleNext: (index: number) => void;
   index: number;
+  completedDeck: number[];
   children?: ReactNode;
-}> = ({ index, children, title, showNext }) => {
+}> = ({ index, children, title, showNext, handleNext, completedDeck }) => {
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    if (completedDeck.includes(index)) setIsVisible(false);
+  }, [completedDeck, index]);
   return (
     <div
       className={`flex flex-col items-center absolute bg-white h-96 ${
-        index === 0 ? "top-0" :
-        index === 1 ? "top-1" :
-        index === 2 ? "top-2" :
-        index === 3 ? "top-3" :
-        index === 4 ? "top-4" :
-        "top-5" 
-      } border p-5 m-5 rounded-t-lg border-black left-0 right-0 bottom-0`}
+        index === 0
+          ? "top-0"
+          : index === 1
+          ? "top-1"
+          : index === 2
+          ? "top-2"
+          : index === 3
+          ? "top-3"
+          : index === 4
+          ? "top-4"
+          : "top-5"
+      } border p-5 m-5 rounded-t-lg border-black left-0 right-0 bottom-0
+      ${!isVisible && "hidden"}
+      `}
     >
+      <h2>{title}</h2>
       {!showNext ? (
-        <>
-          <h2>{title}</h2>
-          {children}
-        </>
+        <>{children}</>
       ) : (
-        <Button>Next</Button>
+        <Button onClick={() => handleNext(index)}>Next</Button>
       )}
     </div>
   );
--- components/steps/CardContainer.tsx
+++ components/steps/CardContainer.tsx
@@ -1,25 +1,49 @@
 import React, { FunctionComponent, useState } from "react";
 import Card from "./Card";
 import ImageContainer from "../ImageContainer";
+import Button from "../Button";
 
-const CardContainer:FunctionComponent<{
-    imageCallback:(images:File[])=>void
-}> = ({imageCallback}) => {
+const CardContainer: FunctionComponent<{
+  imageCallback: (images: File[]) => void;
+}> = ({ imageCallback }) => {
   const [fundas, setFundas] = useState<File[]>([]);
-
+  const [completedDeck, setCompletedDeck] = useState<number[]>([]);
   const updateFileCb = (filesArray: File[]) => {
     console.log("updateFileCb", filesArray);
     setFundas(filesArray);
-    imageCallback(filesArray)
+    imageCallback(filesArray);
   };
+
+  const handleNext = (index: number) => {
+    console.log("handleNext", index);
+    setCompletedDeck((prev) => [...prev, index]);
+  };
+
   return (
     <div className="relative w-full h-full p-5 ">
-      <Card index={0} />
-      <Card index={1} />
-      <Card index={2} />
-      <Card index={3} />
-      <Card title="Crop Roi" index={4} />
-      <Card showNext={fundas.length>0} title="Select Image" index={5}>
+      <Card completedDeck={completedDeck} handleNext={handleNext} index={0} />
+      <Card completedDeck={completedDeck} handleNext={handleNext} index={1} />
+      <Card completedDeck={completedDeck} handleNext={handleNext} index={2} />
+      <Card completedDeck={completedDeck} handleNext={handleNext} index={3} />
+      <Card
+        completedDeck={completedDeck}
+        handleNext={handleNext}
+        title="Crop Roi"
+        index={4}
+      >
+        <p>
+          Place region of interest in the square.
+        </p>
+        <p>Once ready use the crop button</p>
+        <Button>Crop</Button>
+      </Card>
+      <Card
+        completedDeck={completedDeck}
+        handleNext={handleNext}
+        showNext={fundas.length > 0}
+        title="Select Image"
+        index={5}
+      >
         <ImageContainer
           instructions="Drop Your Image Here OR"
           multiple={false}
