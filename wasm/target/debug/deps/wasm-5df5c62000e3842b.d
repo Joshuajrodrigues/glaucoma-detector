884fc8c81c669e73dbc9a9bb1bac4e2166710e6c
Joshua <jrodrigues@teknorix.com>
2023-01-25 12:38:06 +0530

    wasm init

---
+++ wasm/target/debug/deps/wasm-5df5c62000e3842b.d
@@ -0,0 +1,5 @@
+c:\Users\jrodrigues\Desktop\joshua\glaucoma-detector\wasm\target\debug\deps\wasm-5df5c62000e3842b.rmeta: src\lib.rs
+
+c:\Users\jrodrigues\Desktop\joshua\glaucoma-detector\wasm\target\debug\deps\wasm-5df5c62000e3842b.d: src\lib.rs
+
+src\lib.rs:
