0a436d35302a1083ec7509d38d850f2f34699fc5
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-02-09 19:56:19 +0530

    clusters

--- components/ReProcess.tsx
+++ components/ReProcess.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { ImageToShowType } from "../store/useDisaplyResult";
 import Button from "./Button";
 import fuzzy from "../utils/fuzzy";
@@ -10,28 +10,42 @@ const ReProcess: FC<{
   const diskImageData = useProcessedData((s) => s.diskImageData);
   const cupImageData = useProcessedData((s) => s.cupImageData);
   const setImageData = useProcessedData((s) => s.setImageData);
+
+  const [isReprocessed,setIsReprocessed] = useState(false)
   const handleReprocess = () => {
     if (imageType==="disk" && diskImageData) {
       let cluster1 = new ImageData(
         fuzzy(diskImageData.data).cluster1,
         diskImageData.width,
         diskImageData.height
-      );
-      setImageData("diskImageData", cluster1);
-    }
-    if (imageType==="cup" && cupImageData) {
+        );
+        setImageData("diskImageData", cluster1);
+      }
+      if (imageType==="cup" && cupImageData) {
         let cluster1 = new ImageData(
           fuzzy(cupImageData.data).cluster1,
           cupImageData.width,
           cupImageData.height
-        );
-        setImageData("cupImageData", cluster1);
-      }
-  };
+          );
+          setImageData("cupImageData", cluster1);
+        }
+        setIsReprocessed(true)
+      };
 
   return (
     <div className=" font-normal">
      Not what youre looking for ? <button className=" font-bold underline " onClick={handleReprocess}>Reprocess {imageType}</button>
+    {
+      isReprocessed &&
+      <div >
+      <button className="underline m-2 p-1">
+        Cluster 1
+      </button>
+      <button className="underline m-2 p-1">
+        Cluster 2
+      </button>
+      </div>
+    }
     </div>
   );
 };
